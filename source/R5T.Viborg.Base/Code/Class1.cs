366ecc866356e2eb2200053fc58012bec5933a19
MinexAutomation <minex.automation@gmail.com>
2019-11-18 15:04:50 -0800

    Added Selenium web-driver provider service definition.

--- source/R5T.Viborg.Base/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Viborg.Base
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Viborg.Base/Code/Services/Interfaces/IWebDriverProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+using OpenQA.Selenium;
+
+
+namespace R5T.Viborg
+{
+    public interface IWebDriverProvider
+    {
+        IWebDriver GetWebDriver();
+    }
+}

77ac588c2a89a9d5347643f620cca2a30b619b7b
davidcoats <david@getsafetycone.com>
2022-04-27 15:37:31 -0700

    Interim changes.

---
+++ source/R5T.Viborg.Base/Code/Services/Definitions/IWebDriverProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using OpenQA.Selenium;
+
+using R5T.T0064;
+
+
+namespace R5T.Viborg
+{
+    [ServiceDefinitionMarker]
+    public interface IWebDriverProvider : IServiceDefinition
+    {
+        IWebDriver GetWebDriver();
+    }
+}
